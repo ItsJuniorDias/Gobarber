7d3d672cad30fb0af85ee104fbf68611aeda815b
Alexandre <juniordias_@live.com>
2019-10-28 21:16:09 -0300

    user registration

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from "express";
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get("/", async (req, res) => {
-    const user = await User.create({
-        name: 'Junior Dias',
-        email: 'juniordias_@live.com',
-        password_hash: '122345'
-    });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
