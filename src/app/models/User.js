92c1b734110e82f8c477b43961ed8bd647f4af10
Alexandre <juniordias_@live.com>
2019-10-30 11:33:33 -0300

    return middleware auth

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,5 +1,5 @@
-import Sequelize, { Model } from "sequelize";
-import bcrypt from "bcryptjs";
+import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -9,14 +9,14 @@ class User extends Model {
         email: Sequelize.STRING,
         password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
-        provider: Sequelize.BOOLEAN
+        provider: Sequelize.BOOLEAN,
       },
       {
-        sequelize
+        sequelize,
       }
     );
 
-    this.addHook("beforeSave", async user => {
+    this.addHook('beforeSave', async user => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
-routes.put('/users', UserController.update);
+routes.put('/users', authMiddleware, UserController.update);
 
 export default routes;
