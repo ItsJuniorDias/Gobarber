6999cfeb2e6b5243ef81bc604e06c1200ae12af0
itsjuniordias <juniordias_@live.com>
2019-12-18 19:43:20 -0300

    Eslint prettier and config-eslint-prettier

--- src/app.js
+++ src/app.js
@@ -1,19 +1,21 @@
-import  express from 'express';
-import  routes from './routes' ;
+import express from 'express';
+import routes from './routes';
 
 class App {
   constructor() {
     this.server = express();
-    
+
     this.middlewares();
     this.routes();
   }
-    middlewares() {
-      this.server.use(express.json());
-    }
-    routes() {
-      this.server.use(routes);
-    }
+
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
 
-export default new App().server; 
+export default new App().server;
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello Mundo!!' })
-})
+routes.get('/', (req, res) => res.json({ message: 'Hello Mundo!!' }));
 
 export default routes;
