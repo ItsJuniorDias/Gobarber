961ac1987e595fb076043cdf6ad58160e9af8a83
itsjuniordias <juniordias_@live.com>
2020-01-27 12:06:22 -0300

    Updates files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,26 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": [
-        "airbnb-base", "plugin:prettier/recommended"
-    ],
-    "plugins": ["prettier"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "prettier/prettier": "error",
-        "class-methods-use-this": "off",
-        "no-param-reassign": "off",
-        "camelcase": "off",
-        "no-unused-vars": ["error", {"argsIgnorePattern": "next"}],
-    },
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase:": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next"}],
+    "linebreak-style": 0,
+    "global-require": 0,
+    "eslint linebreak-style": [0, "error", "windows"],
+  },
 };
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,7 @@
 import { Router } from 'express';
-import User from './app/models/User';
-
-import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.post('/users', UserController.store);
- 
+routes.get('/', (req, res) => res.json({ message: 'Hello World!!' }));
+
 export default routes;
