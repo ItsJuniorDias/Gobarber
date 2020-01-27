b7bb0a3f8e59b9c96beb66c2c73a799a8dfb3c47
itsjuniordias <juniordias_@live.com>
2020-01-27 14:45:51 -0300

    Loader User Success

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World!!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Alexandre Junior',
+    email: 'juniordias_@live.com',
+    password_hash: '12343232',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
