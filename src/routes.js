f8842ae2d18d0378a9bdfe22b2c274c331fb3b55
itsjuniordias <juniordias_@live.com>
2020-01-28 20:17:11 -0300

    Create User again

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Alexandre Junior',
-    email: 'juniordias_@live.com',
-    password_hash: '12343232',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
