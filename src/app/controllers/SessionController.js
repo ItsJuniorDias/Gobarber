bc28ac833a52591e295fef9635fd769defc55c17
itsjuniordias <juniordias_@live.com>
2020-01-28 20:47:42 -0300

    Autentication JWT

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, '51c72c2c799796880a2369f79386d9c3', {
+        expiresIn: '3d',
+      }),
+    });
+  }
+}
+
+export default new SessionController();
