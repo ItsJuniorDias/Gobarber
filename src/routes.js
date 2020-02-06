d96102014188c5ff419d1ccc9154e509a01619e8
ItsJuniorDias <juniordias_@live.com>
2020-02-06 10:27:17 -0300

    Loader de Users

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello Mundo !!! ' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Alexandre Junior',
+    email: 'juniordias_@live.com',
+    password_hash: '12345',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
