9b13564dc85ee9129959e4df1dc538b0900ee01e
ItsJuniorDias <juniordias_@live.com>
2020-02-05 21:59:17 -0300

    eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "linebreak-style": 0,
+    "global-require": 0,
+    "class-methods-use-this": "off",
+    "no-params-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", {"argsIgnorePattern": "next" }], 
+  },
+};
