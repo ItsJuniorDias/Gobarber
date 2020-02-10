49221528df4d5265c583630e9be3a51148fd4c0c
ItsJuniorDias <juniordias_@live.com>
2020-02-09 21:05:41 -0300

    Loader User refatoring

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,17 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res.status(400).json({ error: 'User already Exists.' });
+    }
+
+    const user = await User.create(req.body);
+
+    return res.json(user);
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,16 @@
 import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+
 import User from './app/models/User';
 
 const routes = new Router();
 
 routes.get('/', async (req, res) => {
   const user = await User.create({
-    name: 'Alexandre Junior',
-    email: 'juniordias_@live.com',
-    password_hash: '12345',
+    name: 'Alexandreee',
+    email: 'juniordias_@',
+    password_hash: '123444',
   });
 
   return res.json(user);
