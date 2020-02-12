de71f186027feb546c84a24b9a6f87afd3cd404f
ItsJuniorDias <juniordias_@live.com>
2020-02-12 19:07:14 -0300

    Craete method Update

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+routes.put('/users', UserController.update);
 routes.post('/sessions', SessionController.store);
 
 export default routes;
