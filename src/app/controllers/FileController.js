7c2a121072f646013c2b9c0fd894a191235803a6
ItsJuniorDias <juniordias_@live.com>
2020-02-13 19:45:58 -0300

    File Controller const file

--- src/app/controllers/FileController.js
+++ src/app/controllers/FileController.js
@@ -2,7 +2,14 @@ import File from '../models/File';
 
 class FileController {
   async store(req, res) {
-    return res.json(req.file);
+    const { originalname: name, filename: path } = req.file;
+
+    const file = await File.create({
+      name,
+      path,
+    });
+
+    return res.json(file);
   }
 }
 
