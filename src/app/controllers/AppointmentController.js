7f71174d777b7468f7c10c2e701379e416728cd9
ItsJuniorDias <juniordias_@live.com>
2020-02-15 12:02:11 -0300

    List Appointment Create

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -4,6 +4,14 @@ import User from '../models/User';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+  async index(req, res) {
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
