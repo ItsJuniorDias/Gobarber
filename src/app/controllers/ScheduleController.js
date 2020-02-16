fdec187d125ba960cddab735744e7e2c6b92c8fc
ItsJuniorDias <juniordias_@live.com>
2020-02-16 10:59:55 -0300

    Feature Schedule Finish

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,3 +1,5 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
 import User from '../models/User';
 import Appointment from '../models/Appointment';
 
@@ -12,8 +14,21 @@ class ScheduleController {
     }
 
     const { date } = req.query;
+    const parsedDate = parseISO(date);
 
-    return res.json({ date });
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parsedDate), endOfDay(parsedDate)],
+        },
+      },
+
+      order: ['date'],
+    });
+
+    return res.json(appointments);
   }
 }
 
