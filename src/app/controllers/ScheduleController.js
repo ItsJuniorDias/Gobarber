34260a950ea0023f0e4165ab2634e9902262cad4
ItsJuniorDias <juniordias_@live.com>
2020-02-16 10:52:48 -0300

    List date today schedule

---
+++ src/app/controllers/ScheduleController.js
@@ -0,0 +1,20 @@
+import User from '../models/User';
+import Appointment from '../models/Appointment';
+
+class ScheduleController {
+  async index(req, res) {
+    const checkUserProvider = await User.findOne({
+      where: { id: req.userId, provider: true },
+    });
+
+    if (!checkUserProvider) {
+      return res.status(401).json({ error: 'User is not a provider' });
+    }
+
+    const { date } = req.query;
+
+    return res.json({ date });
+  }
+}
+
+export default new ScheduleController();
