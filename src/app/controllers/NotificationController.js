478d879e0c5bec0592b90a58971bdeec895108ce
ItsJuniorDias <juniordias_@live.com>
2020-02-17 20:44:02 -0300

    Method check is provider

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -1,7 +1,21 @@
 import Notification from '../schemas/Notification';
+import User from '../models/User';
 
 class NotificationController {
   async index(req, res) {
+    const checkIsProvider = await User.findOne({
+      where: {
+        id: req.userId,
+        provider: true,
+      },
+    });
+
+    if (!checkIsProvider) {
+      return res.status(401).json({
+        error: 'Only provider can load notifications',
+      });
+    }
+
     return res.json();
   }
 }
