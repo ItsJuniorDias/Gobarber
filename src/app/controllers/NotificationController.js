e91034589553c1361e2060ab69f7180b0deae1a8
ItsJuniorDias <juniordias_@live.com>
2020-02-17 20:50:59 -0300

    List order notification

--- src/app/controllers/NotificationController.js
+++ src/app/controllers/NotificationController.js
@@ -19,7 +19,7 @@ class NotificationController {
     const notifications = await Notification.find({
       user: req.userId,
     })
-      .sort('createdAt')
+      .sort({ createdAt: 'desc' })
       .limit(20);
 
     return res.json(notifications);
