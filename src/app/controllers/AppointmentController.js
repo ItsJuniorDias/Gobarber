2ae36497d89b0cfee482cf0bd2c22ad5bc447d4b
ItsJuniorDias <juniordias_@live.com>
2020-02-16 22:13:19 -0300

    new notification appointment

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -94,7 +94,7 @@ class AppointmentController {
     );
 
     await Notification.create({
-      content: `Novo agendamento de ${user.name} para dia ${formattedDate}`,
+      content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
 
