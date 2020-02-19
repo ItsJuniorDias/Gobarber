f1a7c0aa3a9b51164f0dbe8736391523ce3b1d24
ItsJuniorDias <juniordias_@live.com>
2020-02-18 21:01:30 -0300

    Mailtrap customize email

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -111,6 +111,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -135,7 +140,14 @@ class AppointmentController {
     await Mail.sendMail({
       to: `${appointment.provider.name} <${appointment.provider.email}>`,
       subject: 'Schedule canceled',
-      text: 'You have new canceled',
+      template: 'cancellation',
+      context: {
+        provider: appointment.provider.name,
+        user: appointment.user.name,
+        date: format(appointment.date, "'dia' dd 'de' MMMM', ás' H:mm'h' ", {
+          locale: pt,
+        }),
+      },
     });
 
     return res.json(appointment);
