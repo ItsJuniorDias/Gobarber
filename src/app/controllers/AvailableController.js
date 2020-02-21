05c2fdc00e94166235b40113aeafdc4948d31c7b
ItsJuniorDias <juniordias_@live.com>
2020-02-20 21:15:35 -0300

    Available Controller

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,6 +1,28 @@
+import { startOfDay, endOfDay } from 'date-fns';
+import { Op } from 'sequelize';
+import Appointment from '../models/Appointment';
+
 class AvailableController {
   async index(req, res) {
-    return res.json({ ok: true });
+    const { date } = req.query;
+
+    if (!date) {
+      return res.status(400).json({ error: 'Invalid date' });
+    }
+
+    const searchDate = Number(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.params.providerId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(searchDate), endOfDay(searchDate)],
+        },
+      },
+    });
+
+    return res.json(appointments);
   }
 }
 
