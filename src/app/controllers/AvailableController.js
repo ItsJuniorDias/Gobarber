24a28683a956d27b5deebc3d787426dd4bc333ec
ItsJuniorDias <juniordias_@live.com>
2020-02-20 21:27:28 -0300

    update available

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -1,4 +1,12 @@
-import { startOfDay, endOfDay } from 'date-fns';
+import {
+  startOfDay,
+  endOfDay,
+  setHours,
+  setMinutes,
+  setSeconds,
+  format,
+  isAfter,
+} from 'date-fns';
 import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 
@@ -22,7 +30,36 @@ class AvailableController {
       },
     });
 
-    return res.json(appointments);
+    const schedule = [
+      '08:00',
+      '09:00',
+      '10:00',
+      '11:00',
+      '12:00',
+      '13:00',
+      '14:00',
+      '15:00',
+      '16:00',
+      '17:00',
+      '18:00',
+      '19:00',
+    ];
+
+    const available = schedule.map(time => {
+      const [hour, minute] = time.split(':');
+      const value = setSeconds(
+        setMinutes(setHours(searchDate, hour), minute),
+        0
+      );
+
+      return {
+        time,
+        value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
+        available: isAfter(value, new Date()),
+      };
+    });
+
+    return res.json(available);
   }
 }
 
