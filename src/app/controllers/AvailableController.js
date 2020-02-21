e752eb4eaa5426fc78a7d2e9e229ab12ccb1583e
ItsJuniorDias <juniordias_@live.com>
2020-02-20 21:32:10 -0300

    List hours available

--- src/app/controllers/AvailableController.js
+++ src/app/controllers/AvailableController.js
@@ -55,7 +55,9 @@ class AvailableController {
       return {
         time,
         value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
-        available: isAfter(value, new Date()),
+        available:
+          isAfter(value, new Date()) &&
+          !appointments.find(a => format(a.date, 'HH:mm') === time),
       };
     });
 
