e4c585b5c8b5a52f83392a55e6b2a208b27dc978
ItsJuniorDias <juniordias_@live.com>
2020-02-25 12:39:58 -0300

    Config Sentry.io

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import path from 'path';
+import Youch from 'youch';
 import * as Sentry from '@sentry/node';
+import 'express-async-errors';
+
 import routes from './routes';
 import sentryConfig from './config/sentry';
-
-import 'express-async-errors';
 
 import './database';
 
@@ -16,6 +17,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.exceptionHandler();
   }
 
   middlewares() {
@@ -30,6 +32,14 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+
+      return res.status(500).json(errors);
+    });
   }
 }
 
