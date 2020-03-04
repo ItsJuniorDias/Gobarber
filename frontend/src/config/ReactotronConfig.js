4785ac8b57e36382d2ba57f638c816ecf8c85157
ItsJuniorDias <juniordias_@live.com>
2020-03-04 19:43:46 -0300

    Config Saga and Store

--- frontend/src/config/ReactotronConfig.js
+++ frontend/src/config/ReactotronConfig.js
@@ -1,11 +1,16 @@
 import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 if (process.env.NODE_ENV === 'development') {
-  const tron = Reactotron.configure().connect({
-    enabled: true,
-    host: '192.168.0.106', // server ip
-    port: 9090,
-  });
+  const tron = Reactotron.configure()
+    .use(reactotronRedux())
+    .use(reactotronSaga)
+    .connect({
+      enabled: true,
+      host: '192.168.0.106', // server ip
+      port: 9090,
+    });
 
   tron.clear();
 
