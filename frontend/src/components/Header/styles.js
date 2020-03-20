3d8b4c9edf01aa3137207bd597d5db250077dd22
ItsJuniorDias <juniordias_@live.com>
2020-03-20 11:05:11 -0300

    Style Header in process

--- frontend/src/components/Header/styles.js
+++ frontend/src/components/Header/styles.js
@@ -1,7 +1,43 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  background: #fff;
+  padding: 0 30px;
+`;
 
-export const Content = styled.div``;
+export const Content = styled.div`
+  height: 64px;
+  max-width: 900px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 
-export const Profile = styled.div``;
+  nav {
+    display: flex;
+    align-items: center;
+
+    img {
+      margin-right: 20px;
+      padding-right: 20px;
+      border-right: 1px solid #eee;
+    }
+
+    a {
+      font-weight: bold;
+      color: #7159c1;
+    }
+  }
+
+  aside {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  margin-left: 20px;
+  padding-left: 20px;
+  border-left: 1px solid #eee;
+`;
