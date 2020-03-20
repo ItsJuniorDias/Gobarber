beae717c7461032fab9954e9b4ac8e9cd20a53e7
ItsJuniorDias <juniordias_@live.com>
2020-03-20 10:40:10 -0300

    AUTH Requests

--- frontend/src/store/modules/auth/sagas.js
+++ frontend/src/store/modules/auth/sagas.js
@@ -52,7 +52,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp),
 ]);
