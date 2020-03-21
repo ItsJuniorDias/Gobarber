19f2a534a68e77082ebdde2622d6b9dbe610ae06
ItsJuniorDias <juniordias_@live.com>
2020-03-21 20:30:00 -0300

    Finished Style Notification

--- frontend/src/components/Notifications/styles.js
+++ frontend/src/components/Notifications/styles.js
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import { lighten } from 'polished';
 
 export const Container = styled.div`
   position: relative;
@@ -25,6 +26,67 @@ export const Badge = styled.button`
     `}
 `;
 
-export const NotificationList = styled.div``;
+export const NotificationList = styled.div`
+  position: absolute;
+  width: 260px;
+  left: calc(50% - 130px);
+  top: calc(100% + 30px);
+  background: rgba(0, 0, 0, 0.6);
+  border-radius: 4px;
+  padding: 20px;
 
-export const Notification = styled.div``;
+  &::before {
+    content: '';
+    position: absolute;
+    left: calc(50% - 20px);
+    top: -20px;
+    width: 0;
+    height: 0px;
+    border-left: 20px solid transparent;
+    border-right: 20px solid transparent;
+    border-bottom: 20px solid rgba(0, 0, 0, 0.6);
+  }
+`;
+
+export const Notification = styled.div`
+  color: #fff;
+
+  & + div {
+    margin-top: 15px;
+    padding-top: 15px;
+    border-top: 1px solid rgba(255, 255, 255, 0.1);
+  }
+
+  p {
+    font-size: 13px;
+    line-height: 18px;
+  }
+
+  time {
+    font-size: 12px;
+    opacity: 0.6;
+  }
+
+  button {
+    font-size: 12px;
+    border: 0;
+    background: none;
+    color: ${lighten(0.2, '#7159c1')};
+    padding: 0 5px;
+    margin: 0 5px;
+    border-left: 1px solid rgba(255, 255, 255, 0.1);
+  }
+
+  ${props =>
+    props.unread &&
+    css`
+      &::after {
+        content: '';
+        display: inline-block;
+        width: 8px;
+        height: 8px;
+        background: #ff892e;
+        border-radius: 50%;
+      }
+    `}
+`;
