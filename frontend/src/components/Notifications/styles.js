4a94751cb3e53ff4ba714ced2273db4711a6bd35
ItsJuniorDias <juniordias_@live.com>
2020-03-21 20:01:19 -0300

    Styled

--- frontend/src/components/Notifications/styles.js
+++ frontend/src/components/Notifications/styles.js
@@ -1,5 +1,11 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  position: relative;
+`;
 
-export const Badge = styled.button``;
+export const Badge = styled.button`
+  background: none;
+  border: 0;
+  position: relative;
+`;
