4ce30015203ff43abd2235b46691ef5ed25e29f9
ItsJuniorDias <juniordias_@live.com>
2020-03-21 20:12:51 -0300

    Notification List

--- frontend/src/components/Notifications/index.js
+++ frontend/src/components/Notifications/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { MdNotifications } from 'react-icons/md';
 
-import { Container, Badge } from './styles';
+import { Container, Badge, NotificationList, Notification } from './styles';
 
 export default function Notifications() {
   return (
@@ -10,6 +10,24 @@ export default function Notifications() {
       <Badge hasUnread>
         <MdNotifications color="#7159c1" size={20} />
       </Badge>
+
+      <NotificationList>
+        <Notification unread>
+          <p> Você possui um novo agendamento para amanhã</p>
+          <time>há 2 dias </time>
+          <button type="button">Marcar como lida</button>
+        </Notification>
+        <Notification>
+          <p> Você possui um novo agendamento para amanhã</p>
+          <time>há 2 dias </time>
+          <button type="button">Marcar como lida</button>
+        </Notification>
+        <Notification>
+          <p> Você possui um novo agendamento para amanhã</p>
+          <time>há 2 dias </time>
+          <button type="button">Marcar como lida</button>
+        </Notification>
+      </NotificationList>
     </Container>
   );
 }
--- frontend/src/components/Notifications/styles.js
+++ frontend/src/components/Notifications/styles.js
@@ -24,3 +24,7 @@ export const Badge = styled.button`
       }
     `}
 `;
+
+export const NotificationList = styled.div``;
+
+export const Notification = styled.div``;
