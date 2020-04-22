b24cc060211d6240de28fbee653eeeaa303a0ee7
ItsJuniorDias <juniordias_@live.com>
2020-04-21 21:29:58 -0300

    Using useSelector State info User

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
 
 import { Container } from './styles';
 
 export default function Profile() {
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {}
+
   return (
     <Container>
-      <Form>
+      <Form initialData={profile} onSubmit={handleSubmit}>
         <Input name="name" placeholder="Nome Completo" />
         <Input name="email" type="email" placeholder="Seu e-mail" />
 
