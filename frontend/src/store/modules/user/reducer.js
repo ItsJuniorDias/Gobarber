93c10bf61010fdfcda17250490cbb229bef8ddae
ItsJuniorDias <juniordias_@live.com>
2020-04-21 23:39:00 -0300

    Feature Profile

--- frontend/src/store/modules/user/reducer.js
+++ frontend/src/store/modules/user/reducer.js
@@ -11,9 +11,12 @@ export default function user(state = INITIAL_STATE, action) {
         draft.profile = action.payload.user;
         break;
       }
-
       case '@user/UPDATE_PROFILE_SUCCESS': {
         draft.profile = action.payload.profile;
+        break;
+      }
+      case '@auth/SIGN_OUT': {
+        draft.profile = null;
         break;
       }
       default:
