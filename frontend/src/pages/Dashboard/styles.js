fb9402975fefacc8fcb32afbc2f00e19a14211e7
ItsJuniorDias <juniordias_@live.com>
2020-04-23 00:01:27 -0300

    Style Dashboard finished

--- frontend/src/pages/Dashboard/styles.js
+++ frontend/src/pages/Dashboard/styles.js
@@ -35,4 +35,20 @@ export const Container = styled.div`
 export const Time = styled.li`
   padding: 20px;
   border-radius: 4px;
+  background: #fff;
+
+  opacity: ${props => (props.past ? 0.6 : 1)};
+
+  strong {
+    display: block;
+    color: ${props => (props.available ? '#999' : '#7159c1')};
+    font-size: 20px;
+    font-weight: normal;
+  }
+
+  span {
+    display: block;
+    margin-top: 3px;
+    color: ${props => (props.available ? '#999' : '#666')};
+  }
 `;
