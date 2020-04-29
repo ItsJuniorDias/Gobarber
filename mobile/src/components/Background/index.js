e71a8ca1babf25add66393006aa944dd8bc8bbf6
ItsJuniorDias <juniordias_@live.com>
2020-04-28 22:36:04 -0300

    Import icon

--- mobile/src/components/Background/index.js
+++ mobile/src/components/Background/index.js
@@ -2,7 +2,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
 export default styled(LinearGradient).attrs({
-  colors: ['#7159c1', '#ab59c1']
+  colors: ['#9708cc', '#43cbff']
 })`
   flex: 1;
 `;
--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import Input from '~/components/Input';
 import Background from '~/components/Background';
 
 // import { Container } from './styles';
@@ -9,6 +10,7 @@ export default function SignIn() {
   return (
     <Background>
     <Text>SignIn</Text>
+     <Input style={{ marginTop: 30}} icon="call" placeholder="Digite seu nome" />
     </Background>
   );
 }
