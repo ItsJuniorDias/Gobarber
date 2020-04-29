03a5bc26d6663b1cd4f9e3c13279a18b4d7ed67c
ItsJuniorDias <juniordias_@live.com>
2020-04-28 22:55:10 -0300

    Components Input and Button

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text } from 'react-native';
 
 import Input from '~/components/Input';
+import Button from '~/components/Button';
 import Background from '~/components/Background';
 
 // import { Container } from './styles';
@@ -10,7 +11,13 @@ export default function SignIn() {
   return (
     <Background>
     <Text>SignIn</Text>
-     <Input style={{ marginTop: 30}} icon="call" placeholder="Digite seu nome" />
+     <Input
+      style={{ marginTop: 30}}
+      icon="call"
+      placeholder="Digite seu nome"
+     />
+
+     <Button>Entrar</Button>
     </Background>
   );
 }
