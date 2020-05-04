7d3aadbb6fd072756ab869fec05204fa4f3e26ca
ItsJuniorDias <juniordias_@live.com>
2020-05-03 21:51:08 -0300

    Config Reactotron

---
+++ mobile/src/config/ReactotronConfig.js
@@ -0,0 +1,16 @@
+import Reactotron from 'reactotron-react-native';
+import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotro-redux-saga';
+
+if(__DEV__) {
+  const tron = Reactotron
+  .configure({ host: '192.168.0.103' })
+  .useReactNative()
+  .use(reactotronRedux())
+  .use(reactotronSaga())
+  .connect();
+
+  tron.clear();
+
+  console.tron = tron;
+}
--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import './config/ReactotronConfig';
+
 import Routes from '~/routes';
+
 
 export default function src() {
   return (
