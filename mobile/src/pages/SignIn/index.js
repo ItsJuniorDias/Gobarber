1fbdd12fec9b57c2efadd2e5c33a7baedd973262
ItsJuniorDias <juniordias_@live.com>
2020-05-03 21:34:29 -0300

    Accessibility

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { Image } from 'react-native';
 
 import logo from '~/assets/logo.png';
@@ -16,6 +16,14 @@ import {
    from './styles';
 
 export default function SignIn({ navigation }) {
+  const passwordRef = useRef();
+
+  function handleSubmit() {
+
+  }
+
+
+
   return (
     <Background>
       <Container>
@@ -28,15 +36,20 @@ export default function SignIn({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
 
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
 
-          <SubmitButton onPress={() => {}}>
+          <SubmitButton onPress={handleSubmit}>
             Acessar
           </SubmitButton>
         </Form>
--- mobile/src/pages/SignUp/index.js
+++ mobile/src/pages/SignUp/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { Image } from 'react-native';
 
 import logo from '~/assets/logo.png';
@@ -16,6 +16,13 @@ import {
    from './styles';
 
 export default function SignUp({ navigation }) {
+  const passwordRef = useRef();
+  const emailRef = useRef();
+
+  function handleSubmit() {
+
+  }
+
   return (
     <Background>
       <Container>
@@ -27,6 +34,8 @@ export default function SignUp({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu nome completo"
+            returnKeyType="next"
+            onSubmitEditing={() => emailRef.current.focus()}
           />
 
           <FormInput
@@ -35,21 +44,27 @@ export default function SignUp({ navigation }) {
             autoCorrect={false}
             autoCapitalize="none"
             placeholder="Digite seu e-mail"
+            ref={emailRef}
+            returnKeyType="next"
+            onSubmitEditing={() => passwordRef.current.focus()}
           />
 
           <FormInput
             icon="lock-outline"
             secureTextEntry
             placeholder="Sua senha secreta"
+            ref={passwordRef}
+            returnKeyType="send"
+            onSubmitEditing={handleSubmit}
           />
 
-          <SubmitButton onPress={() => {}}>
+          <SubmitButton onPress={handleSubmit}>
             Acessar
           </SubmitButton>
         </Form>
 
         <SignLink onPress={() => navigation.navigate('SignIn')}>
-          <SignLinkText>Já tenha uma conta</SignLinkText>
+          <SignLinkText>Já tenho conta</SignLinkText>
         </SignLink>
       </Container>
     </Background>
